155c625aba40985b2b09324feadb43d4c7f641a2
victor <victoorsantos266@gmail.com.br>
2024-08-18 11:04:06 -0300

    refact: vibingcontroller logic and readability

--- app/Http/Controllers/VibingController.php
+++ app/Http/Controllers/VibingController.php
@@ -5,61 +5,60 @@ namespace App\Http\Controllers;
 use App\Models\Album;
 use App\Models\Artist;
 use App\Models\Playlist;
-use App\Models\Song;
 use Illuminate\Http\Request;
 
 class VibingController extends Controller
 {
-    public function showAlbumSongs(Request $request, $albumId = null, $artistId = null)
+    public function showAlbumSongs(Request $request, $albumId = null)
     {
-  
-        if ($albumId) {
+        /** @var Album|null $album */
+        $album = Album::find($albumId);
+        if ($albumId && $album) {
             $idColumn = 'album_id';
             $idValue = $albumId;
-            $title = 'Listagem de Musicas Do Album: ' . Album::find($albumId)->name ?? '';
-        
-        } else {
-           return;
+            $title = 'Listagem de Musicas Do Album: ' . $album->name;
+            return view('pages.vibing.songs', [
+                'idColumn' => $idColumn,
+                'idValue' => $idValue,
+                'title' => $title
+            ]);
         }
-
-        return view('pages.vibing.songs', [
-            'idColumn' => $idColumn,
-            'idValue' => $idValue,
-            'title' => $title
-        ]);
+        return redirect()->back();
     }
 
     public function showArtistSongs(Request $request, $artistId = null)
     {
-        if ($artistId) {
+        /** @var Artist|null $artist */
+        $artist = Artist::find($artistId);
+
+        if ($artistId && $artist) {
             $idColumn = 'artist_id';
             $idValue = $artistId;
-            $title = 'Listagem de Musicas Do Cantor: ' . Artist::find($artistId)->name ?? '';
+            $title = 'Listagem de Musicas Do Cantor: ' . $artist->name;
+            return view('pages.vibing.songs', [
+                'idColumn' => $idColumn,
+                'idValue' => $idValue,
+                'title' => $title
+            ]);
 
-        } else {
-            redirect()->back();
         }
-        return view('pages.vibing.songs', [
-            'idColumn' => $idColumn,
-            'idValue' => $idValue,
-            'title' => $title
-        ]);
+        return redirect()->back();
     }
 
     public function showPlaylistSongs(Request $request, $playlistId = null)
     {
-        if ($playlistId) {
+        /** @var Playlist|null $playlist */
+        $playlist = Playlist::find($playlistId);
+        if ($playlistId && $playlist) {
             $idColumn = 'playlist_id';
             $idValue = $playlistId;
-            $title = 'Listagem de Musicas Da Playlist: ' . Playlist::find($playlistId)->name ?? '';
-
-        } else {
-            redirect()->back();
-        }
-        return view('pages.vibing.songs', [
-            'idColumn' => $idColumn,
-            'idValue' => $idValue,
-            'title' => $title
-        ]);
+            $title = 'Listagem de Musicas Da Playlist: ' . $playlist->name;
+            return view('pages.vibing.songs', [
+                'idColumn' => $idColumn,
+                'idValue' => $idValue,
+                'title' => $title
+            ]);
+        } 
+        return redirect()->back();
     }
 }
